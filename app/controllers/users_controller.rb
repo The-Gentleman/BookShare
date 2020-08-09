3c8cd91244ad36ac59e6a95c290b69e8c66e882f
The-Gentleman <zraja4402@gmail.com>
2020-08-09 12:10:47 -0400

    added user controller + authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+
+    get '/login' do 
+        erb :'/login'
+    end 
+
+    post '/login' do 
+        @user = User.find_by_username(params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect to "/users/#{@user.id}"
+        else 
+            redirect to '/login'
+        end 
+    end 
+
+    get '/users/:id' do 
+        @user = User.find_by_id(params[:id])
+        erb :'/users/show'
+    end 
+
+    get '/signup' do 
+        erb :'/users/signup'
+    end 
+
+    post '/signup' do 
+        @user = User.create(params)
+        session[:id] = @user.id
+        redirect to "/login"
+    end 
+
+    get '/logout' do 
+        session.clear
+        redirect to '/'
+    end 
+end 
