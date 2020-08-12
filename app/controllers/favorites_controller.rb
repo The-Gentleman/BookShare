8c010b3b8d80ed42550edf1bf68897aea8fdc9ff
The-Gentleman <zraja4402@gmail.com>
2020-08-12 12:09:05 -0400

    debugged duplicate issue

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,16 +6,12 @@ class FavoritesController < ApplicationController
     end 
 
     post '/favorites' do 
-        # iterate through params hash
-        # access title ids to create favorite
-        params.each do |k,v|
-            if v.class == Array
-                v.each do |id|
-                    Favorite.find_or_create_by(user_id: current_user.id, book_id: id.to_i, comments: params[:comments])
-                end 
-            end 
-        end
-        redirect to "/favorites/show"
+        ids = params[:title_ids]
+        ids.each do |id|
+            favorite = Favorite.find_or_create_by(user_id: current_user.id, book_id: id.to_i)
+            favorite.update(comments: params[:comments])
+        end 
+        redirect to '/favorites/show'
     end 
 
 #               READ
@@ -26,18 +22,6 @@ class FavoritesController < ApplicationController
     end 
 #               UPDATE 
 
-    # get  '/favorites/:id/edit' do
-    #     @favorite = Favorite.find(params[:id])
-    #     erb :'/favorites/edit'
-    # end 
-
-    # patch '/favorites/:id' do 
-    #     @favorite = Favorite.find(params[:id])
-    #     params[:title_ids].each do |id|
-    #         @favorite.update(user_id: current_user.id, book_id: id)
-    #     end 
-    #     redirect to "/favorites/show"
-    # end 
 
 
 end 
